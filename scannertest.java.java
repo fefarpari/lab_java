435252e6e336fdb7579e703ad403cb0463bff4d6
fefarpari <fefarpari67@gmail.com>
2026-07-10 08:17:17 +0530

    Add files via upload

---
+++ scannertest.java.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class scannertest
+{
+		public static void main(String args[])
+		{
+				Scanner sc=new Scanner(System.in);
+				system.out.println ("Enter your rollno : ");
+				int rollno=sc.nextInt();
+				system.out.println ("Enter name : ");
+				string nm=sc.next();
+				system.out.println ("Enter Fees : ");
+				double f=sc.nextDouble();
+				system.out.println ("Enter stream : ");
+				string s=sc.next();
+				system.out.println ("Rollno ="+rollno+ "\n Name ="+nm+
+				"\n Fees ="+f+ "\n stream ="+s);
+				
+		}
+}
