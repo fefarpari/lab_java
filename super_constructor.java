aaf7c74a08ddc390ca9eb674e0285abc197f72ac
fefarpari <fefarpari67@gmail.com>
2026-08-04 09:15:32 +0530

    Add files via upload

---
+++ super_constructor.java
@@ -0,0 +1,22 @@
+class Fruite
+{
+	Fruite()
+	{
+		System.out.println(" I am super class constructor fruite");
+	}
+}
+class Mango extends Fruite
+{
+	Mango()
+	{
+		super();
+		System.out.println("I am a sub class mango");
+	}
+}
+class super_constructor
+{
+	public static void main(String[] args)
+	{
+		Mango m=new Mango();
+	}
+}
